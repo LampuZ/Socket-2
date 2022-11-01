320c0162f9a69f86bff790d889e76998518abf33
Zxn4 <106302989+Zxn4@users.noreply.github.com>
2022-11-01 11:42:26 +0100

    Update Server.echo.c

--- Server.echo.c
+++ Server.echo.c
@@ -1,108 +1,155 @@
 /**
- * @file server.echo.c
- * @author Jarno Conedera, Matteo Lampugnano
- * @brief usare il plugin di doxygen e inserire il tutto 
+ * @file client.echo.c
+ * @author gabriele chiusano
+ * @brief 
  * @version 0.1
- * @date 
+ * @date 2020-12-02
  * 
- * @copyright copyleft (c) 2022
+ * @copyright copyleft (c) 2020
  * 
  */
 
-/*!
-Verificare i seguenti includes:
-pacchetto `sys`
-- types.h
-- socketh
-pacchetto `netinet`
-- in.h
-controllare ctype.h, unistd.h
-*/
-
-/*
-includes section
-*/
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <errno.h>
 #include <unistd.h>
+
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define SOCKET_ERROR ((int)-1)   // error code
-#define port ((int)5001)           //socket_port
-#define RETURN ((int)13)        //valore ASCII per valore di RETURN/carriage
-#define quit ((int)27)          //valore ASCII per chiudere la connessione
+#define PORT 1234 // server port
+#define SOCKET_ERROR ((int)-1) // error code conversion
+#define END_RECV 27 // ASCII value for escape character (/ BUTTON)
+#define RETURN_CHAR 10
+#define RETURN ((int)13) 
+#define MAX_PROMPT_LENGTH 200
 
-
-int main(unsigned argc, char **argv) //inizializzazione del main
+/**
+ * @brief simulates the prompt of a telnet client using sockets as
+ * input parameters.
+ * 
+ * @param in socket file descriptor integer used to receive stuffs
+ * @param out socket file descriptor integer used to send stuffs
+ */
+void telnetClient(int in, int out)
 {
-        
-    int handshakingSocket, commSocket;  //definizione dei due socket
+    // input string:
+    char * prompt = malloc(MAX_PROMPT_LENGTH);
+    char prompted, recvChar;
+    int len, asciiVal;
 
-    // Last character read from socket
-    int readASCII = 0; //se si avesse voluto prendere stringhe intere, sarebbe stato necessario inizializzare un'array malloc
-    int clientSize = 0;
-
-    struct sockaddr_in server, client; // Struct che contiene i byte
-    server.sin_family = AF_INET; //Definizione famiglia del socket
-    server.sin_addr.s_addr = htonl(INADDR_ANY); //Definizione address
-    server.sin_port = htons(port);  //Definizione porta del socket
-    /** Transport Endpoint definition */
-    if ((handshakingSocket = socket(server.sin_family, SOCK_STREAM, 0)) == SOCKET_ERROR)  //0 = flag 
+    // infinite loop used to simulate interactive prompting
+    do 
     {
-         printf("\nErrore durante l'handshaking\n"); //Stampa dell'errore
-        exit(EXIT_FAILURE); //exit
-    }
-    if (bind( handshakingSocket,(struct sockaddr *) &server, sizeof (server)))  //indirizzo ip con la quale si svolgerà il bind
-    {  
+        printf("Send msg: %c to end transmission\n", END_RECV);
+        scanf("%s",prompt);
+        // ** loop over the 
         
-        printf("\nErrore durante er bind\n");  //stampa dell'errore
-        exit(EXIT_FAILURE);
-    }
-
-    /** Put the server in listening mode */
-    listen(handshakingSocket,1); //socket e numero di client in coda
-    //** debug message
-    printf("Echo Server: ready on port [%d]\n", port);  //Porta in stato ready
-
-    clientSize = sizeof(client); //Definita la "funzione" che definisce la dimensione del client per non avere errori riguardanti la memoria
-
-    if ((commSocket = accept(handshakingSocket,(struct sockaddr *)&client, &clientSize)) == SOCKET_ERROR) {  //permette la connessione di client al socket
-        
-        printf("\nErrore durante l'accept\n"); //errore
-        exit(EXIT_FAILURE);
-    }
-
-    /** after handshaking, send back to client the greetings message, just for testing purposes */
-    send(commSocket, "Hello! Echo server here.\n", 26, 0);
-
-    /** do/while cycle that keeps the communication up */
-    do {
-        if (recv(commSocket, &readASCII , 1 , 0 )) { //0 è una flag ed 1 la dimensione del valore ricevuto
-            int asciiVal = (int) readASCII;    // prende i valori da readASCII e li converte in intero
-
-                if (asciiVal != RETURN && asciiVal != 13)
-                {    
-                    /**
-                     * @brief Stampa dell'output nel socket Server.
-                     */
-
-                    if(isprint(asciiVal)) //
-                        printf("RECV:[%c][%d] Echoing..\n",readASCII , asciiVal);
-                  
-                }
-            // **  echoing back
-
-        send(commSocket,&readASCII,1,0); //Echo Server ha ricevuto il messaggio dal Client
+        for (int i = 0; i < sizeof(prompt) ; i++) 
+        {
+            // ** from prompt
+            char prompted = prompt[i] ;
+            if(prompted!= RETURN_CHAR && prompted!=RETURN){
+            // ** write to server something
+            write(in, &prompted,1);
+            // ** wait for the answer from the server
+            if (read(out, &recvChar,1) > 0) 
+            {
+                asciiVal=(int)recvChar;
+                
+                printf("RECV::[%c][%d]\n", recvChar, (unsigned int)asciiVal);
+                fflush(stdout); // ** force stdout to write buffer
+                
+            }
+            }
         }
-    } while (readASCII != quit);
-    /**
-     * @brief Chiudo i due socket
-     * 
-     */
-    close(commSocket); 
-    close(handshakingSocket);
-    fprintf(stderr, "Server connection closed.\n");
+
+    } while (recvChar != END_RECV); // if arrives / close the connection
+
+    // ** free the malloc'd variables
+    free(prompt);
 }
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <server ip> <server port>\n", argv[0]);
+        exit(1);
+    }
+
+    // Server INFO
+    char * serverAddress = argv[1];
+    // int serverPort = SERVER_PORT;
+    int serverPort = atoi(argv[2]); // FIXME: Warning
+
+    int recvChar;                        // last recv"d char
+    struct sockaddr_in clientConf, serverConf;    // socket data structures
+    int socketClient, socketServer;                // socket identifier
+
+
+    /** Transport Endpoint definition 
+     * 
+     * initialize the socket:
+     * AF_INET
+     * SOCK_STREAM
+     * with default behavior
+     * 
+    */
+    socketClient = socket(AF_INET,SOCK_STREAM,0);
+
+    /** error management: notify the user if something goes horribly wrong */
+    if(socketClient==-1)
+    {
+
+        printf("\nErrore durante la creazione\n"); 
+        return(SOCKET_ERROR);        
+    }
+    
+    clientConf.sin_family = AF_INET;
+    clientConf.sin_addr.s_addr = htonl(INADDR_ANY);
+    clientConf.sin_port = htons(0);
+
+    /** bind address to transport endpoint */
+    /** error management: notify the user if something goes horribly wrong */
+    if (bind( socketClient,(struct sockaddr *)&clientConf, sizeof(clientConf))==-1)
+    {
+        printf("\nErrore durante il bind\n"); 
+        /*
+        gestione errore
+        */
+       return SOCKET_ERROR;
+    }
+
+    serverConf.sin_family = AF_INET ;
+    serverConf.sin_addr.s_addr = inet_addr(serverAddress );
+    serverConf.sin_port = htons(serverPort );
+    
+    /** try to connect the socket to the specified server  */
+    /** error management: notify the user if something goes horribly wrong */
+    
+    if ((socketServer = connect( socketClient,(struct sockaddr *)&serverConf, sizeof(serverConf))) == SOCKET_ERROR) {  //permette la connessione di client al socket
+        
+        printf("\nErrore durante la connect\n"); //errore
+        exit(EXIT_FAILURE);
+    }
+
+    send( socketServer , "Hello! Echo server here.\n", 26, 0);
+
+    /** do some printing in order to notify the user */
+    /** error management: notify the user if something goes horribly wrong */
+
+    /** telnetClient simulation */
+    telnetClient(socketClient, socketServer);
+    
+
+    /* socket::close */
+    printf("\nsocket::close\n");
+    close(socketClient);
+    close(socketServer);
+    return (0);
